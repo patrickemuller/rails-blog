606dbae9020a9b17d505b188a6402ef303309921
Patrick Müller <patrick.e.muller@gmail.com>
2017-06-06 04:16:58 -0300

    Remove body attribute from posts#index of ActiveAdmin

--- app/admin/post.rb
+++ app/admin/post.rb
@@ -1,3 +1,11 @@
 ActiveAdmin.register Post do
   permit_params :slug, :title, :body
+
+  index do
+    id_column
+    column :slug
+    column :title
+    column :created_at
+    actions
+  end
 end
