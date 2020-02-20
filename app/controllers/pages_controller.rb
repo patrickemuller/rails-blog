861583fe016557330a07ed3f58af332afcd2874a
Patrick <patrick.e.muller@gmail.com>
2020-02-19 22:29:49 -0300

    Add rubocop, rubocop-rails, rubocop-rspec & rubocop-performance gems

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class PagesController < ApplicationController
   def homepage
     @posts = Post.all.order(created_at: :desc).page(params[:page]).per(12)
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-print '========> Creating Basic Admins....'
+# frozen_string_literal: true
+
+Rails.logger('========> Creating Basic Admins....')
 
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
 
-print "DONE! \n\n"
+Rails.logger("DONE! \n\n")
 
-print '========> Creating Basic Posts....'
+Rails.logger('========> Creating Basic Posts....')
 
 100.times do |index|
-  Post.create!(slug: "post-#{index}", title: "Post #{index}", body: "Lorem ipsum dolor sit amet")
+  Post.create!(slug: "post-#{index}", title: "Post #{index}", body: 'Lorem ipsum dolor sit amet')
 end
 
-print "DONE! \n\n"
+Rails.logger("DONE! \n\n")
