d8874e014311bf27b5559f6a424b57e18bbe4e30
Patrick <patrick.e.muller@gmail.com>
2020-02-20 16:29:04 -0300

    Fix wrong validate_uniqueness_of for models/post

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe Post, type: :model do
     it { is_expected.to validate_presence_of(:slug) }
     it { is_expected.to validate_presence_of(:body) }
     it { is_expected.to validate_presence_of(:title) }
-    it { is_expected.to uniqueness_presence_of(:slug) }
-    it { is_expected.to uniqueness_presence_of(:title) }
+    it { is_expected.to validate_uniqueness_of(:slug) }
+    it { is_expected.to validate_uniqueness_of(:title) }
   end
 end
