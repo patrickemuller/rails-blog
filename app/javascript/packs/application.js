465c788cccca165616374361a41a9ef037398568
Patrick <patrick.e.muller@gmail.com>
2020-02-21 17:22:57 -0300

    Remove default packs from javascript/packs/application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,8 +14,3 @@
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
